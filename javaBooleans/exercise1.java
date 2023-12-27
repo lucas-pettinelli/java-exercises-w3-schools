6eb9a1b48f29ee2a9f7547283621e8dd0a64ef16
lucas-pettinelli <pettinellilucas@gmail.com>
2023-12-27 20:51:27 -0300

    Exercicios java booleanos

---
+++ javaBooleans/exercise1.java
@@ -0,0 +1,12 @@
+//Fill in the missing parts to print the values true and false:
+
+public class MyClass{
+	public static void main(String[] args){
+	
+	boolean isJavaFun = true;
+	boolean isFishTasty = false;
+	System.out.println(isJavaFun);
+	System.out.println(isFishTasty);
+	
+	}
+}
---
+++ javaBooleans/exercise2.java
@@ -0,0 +1,11 @@
+//Fill in the missing parts to print the value true:
+
+public class MyClass{
+	public static void main(String[] args){
+	
+	int x = 10;
+	int y = 9;
+	System.out.println(x > y);
+	
+	}
+}
