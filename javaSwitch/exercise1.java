c2fae606323d23b1f5993d9840d478f9bd9677a5
lucas-pettinelli <pettinellilucas@gmail.com>
2023-12-30 22:18:57 -0300

    Exercicios java com switch case, break e default

---
+++ javaSwitch/exercise1.java
@@ -0,0 +1,16 @@
+//Insert the missing parts to complete the following switch statement.
+
+public class MyClass{
+	public static void main(String[] args){
+	int day = 2;
+	switch (day) {
+		case 1:
+    		System.out.println("Saturday");
+    		break;
+  
+		case 2:
+	    	System.out.println("Sunday");
+    
+		break;
+	}
+}
---
+++ javaSwitch/exercise2.java
@@ -0,0 +1,21 @@
+//Complete the switch statement, and add the correct keyword at the end to specify some code to run if there is no case match in the switch statement.
+
+public class MyClass{
+	public static void main(String[] args){
+	
+	int day = 4;
+	switch (day) {
+  
+		case 1:
+    		System.out.println("Saturday");
+    		break;
+  
+		case 2:
+    		System.out.println("Sunday");
+    
+		break;
+  
+		default:
+    		System.out.println("Weekend");
+	}
+}
